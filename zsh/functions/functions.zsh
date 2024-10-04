3646e0504c71cde40db6c008654c0af30fa768e8
Julian Hesse <julian.hesse@fu-berlin.de>
2024-10-04 02:40:32 +0200

    switches from deprecated antibody to antidote

--- zsh/functions/functions.zsh
+++ zsh/functions/functions.zsh
@@ -1,9 +1,5 @@
 alias uz=update-zsh
 update-zsh(){
-	ANTIBODY_HOME="$(antibody home)"
-	export ZSH="$ANTIBODY_HOME"/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh
-	DISABLE_AUTO_UPDATE="true"
-	antibody bundle <~/.dotfiles/zsh/plugins.txt >~/.zsh_plugins.sh
-	antibody update
+	antidote bundle <~/.dotfiles/zsh/plugins.txt >~/.zsh_plugins.sh
 	source ~/.zshrc
 }
